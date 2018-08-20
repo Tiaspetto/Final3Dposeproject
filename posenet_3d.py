179b620198bdf051dac04c0ec78f649e0500d291
CubicCodex <xianglichuan@live.com>
2018-08-20 14:49:47 +0100

    fix

--- posenet_3d.py
+++ posenet_3d.py
@@ -54,15 +54,9 @@ def resnet50_32s(input_shape = (224, 224, 3), model_input = ''):
     # add upsampler
     stride = 16
     X = UpSampling2D(size = (int(stride/2), int(stride/2)))(X)
-    X = Conv2D(14, (5, 5), name = 'pred_32s', padding = 'same', kernel_initializer = glorot_uniform(seed=0), kernel_regularizer = regularizers.l2(0.01))(X)
+    X = Conv2D(14, (5, 5), strides = (2, 2) name = 'pred_32s', padding = 'valid', kernel_initializer = glorot_uniform(seed=0), kernel_regularizer = regularizers.l2(0.01))(X)
     X = LeakyReLU(alpha=.001)(X)
     
-    X = Conv2D(256, (3, 3), name = 'pred_32s_down1', padding = 'valid', kernel_initializer = glorot_uniform(seed=0), kernel_regularizer = regularizers.l2(0.01))(X)
-    X = LeakyReLU(alpha=.001)(X)
-    X = Conv2D(512, (3, 3), name = 'pred_32s_down1', padding = 'valid', kernel_initializer = glorot_uniform(seed=0), kernel_regularizer = regularizers.l2(0.01))(X)
-    X = LeakyReLU(alpha=.001)(X)
-    X = Conv2D(1024, (3, 3), name = 'pred_32s_down1', padding = 'valid', kernel_initializer = glorot_uniform(seed=0), kernel_regularizer = regularizers.l2(0.01))(X)
-    X = LeakyReLU(alpha=.001)(X)
     
     # output layer
     X = Dense(42, activation='linear', name='fc_'  + str('pred_32s'), kernel_initializer = glorot_uniform(seed=0), kernel_regularizer = regularizers.l2(0.01))(X)
@@ -77,9 +71,6 @@ def resnet50_32s(input_shape = (224, 224, 3), model_input = ''):
     train_layers = ['pred_32',
                     'pred_32s',
                     'fc_pred_32s',
-                    'pred_32s_down1',
-                    'pred_32s_down2',
-                    'pred_32s_down3',
 
                     'bn4b_branch2c', 
                     'res4b_branch2c',
@@ -121,7 +112,7 @@ def resnet50_16s(input_shape = (224, 224, 3), model_input = ''):
     X = base_model.get_layer('activation_40').output
     X = Conv2D(42, (1, 1), name = 'pred_16', padding = 'valid', kernel_initializer = glorot_uniform(seed=0), kernel_regularizer = regularizers.l2(0.01))(X)
     X = UpSampling2D(name='upsampling_16', size = (int(stride/2), int(stride/2)))(X)
-    X = Conv2D(42, (5, 5), name = 'pred_16s', padding = 'same', kernel_initializer = glorot_uniform(seed=0), kernel_regularizer = regularizers.l2(0.01))(X)
+    X = Conv2D(14, (5, 5), strides = (2, 2), name = 'pred_16s', padding = 'valid', kernel_initializer = glorot_uniform(seed=0), kernel_regularizer = regularizers.l2(0.01))(X)
     
     # merge classifiers
     X = merge([X, base_model.get_layer('pred_32s').output],mode = 'sum')
@@ -186,7 +177,7 @@ def resnet50_8s(input_shape = (224, 224, 3), model_input = ''):
     X = base_model.get_layer('activation_22').output
     X = Conv2D(42, (1, 1), name = 'pred_8', padding = 'valid', kernel_initializer = glorot_uniform(seed=0), kernel_regularizer = regularizers.l2(0.01))(X)
     X = UpSampling2D(name='upsampling_8',size=(int(stride/8), int(stride/8)))(X)
-    X = Conv2D(42, (5, 5), name = 'pred_8s', padding = 'same', kernel_initializer = glorot_uniform(seed=0), kernel_regularizer = regularizers.l2(0.01))(X)
+    X = Conv2D(14, (5, 5), strides = (2, 2), name = 'pred_8s', padding = 'valid', kernel_initializer = glorot_uniform(seed=0), kernel_regularizer = regularizers.l2(0.01))(X)
     
     # merge classifiers
     X = merge([X, base_model.get_layer('pred_16s').output],mode = 'sum')
