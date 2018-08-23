52e9c75b9ec10820f5df745c4e0b4c27584770fc
CubicCodex <xianglichuan@live.com>
2018-08-23 16:34:03 +0100

    fix bug

--- train.py
+++ train.py
@@ -182,7 +182,7 @@ def train_3d():
     model.compile(optimizer=adam, loss=euc_dist_keras,
                   metrics=['mae'])
     #lrate = LearningRateScheduler(step_decay)
-    clr = CyclicLR(base_lr = float("1e-6"), max_lr = float("1e-4"), step_size = 2240, mode 'triangular')
+    clr = CyclicLR(base_lr = float("1e-6"), max_lr = float("1e-4"), step_size = 2240, mode = 'triangular')
     model.summary()
     model.load_weights("model_data/3d_weights-1077.0956.hdf5")
     result = model.fit_generator(generator=pose3d_get_train_batch(train_array, 8, True),
