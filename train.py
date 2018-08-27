a780f11c5afc8c5446af6f047df742cd8128ebb0
CubicCodex <xianglichuan@live.com>
2018-08-27 16:11:34 +0100

    LR

--- train.py
+++ train.py
@@ -286,7 +286,7 @@ def train_3d_8s():
     #lrate = LearningRateScheduler(step_decay)
     clr = CyclicLR(base_lr = float("1e-6"), max_lr = float("1e-3"), step_size = 2069, mode = 'triangular')
     model.summary()
-    #model.load_weights("model_data/3d_weights-238.4385.hdf5")
+    model.load_weights("model_data/3d_weights_8s-0.1714.hdf5")
     result = model.fit_generator(generator=pose3d_get_train_batch(train_array, 8, True),
                                  steps_per_epoch=2069,
                                  callbacks=[ckpt, clr],
