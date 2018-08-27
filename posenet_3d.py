4b73e7a7adfc83b33dedbd8663698be134dd9c6b
CubicCodex <xianglichuan@live.com>
2018-08-27 01:52:53 +0100

    fix bug

--- posenet_3d.py
+++ posenet_3d.py
@@ -126,7 +126,7 @@ def resnet50_16s(input_shape = (224, 224, 3), model_input = ''):
     
     # add 16s classifier
     X = base_model.get_layer('activation_22').output
-    X = Conv2D(14, (1, 1), name = 'pred_16', padding = 'valid', kernel_initializer = glorot_uniform(seed=0), kernel_regularizer = regularizers.l2(0.01))(X)
+    X = Conv2D(42, (1, 1), name = 'pred_16', padding = 'valid', kernel_initializer = glorot_uniform(seed=0), kernel_regularizer = regularizers.l2(0.01))(X)
     X = UpSampling2D(name='upsampling_16', size = (int(stride/2), int(stride/2)))(X)
     X = Conv2D(256, (3, 3), strides = (1, 1), name = 'pred_16s_feature1', padding = 'same', kernel_initializer = glorot_uniform(seed=0), kernel_regularizer = regularizers.l2(0.01))(X)
     X = Activation('tanh')(X)
@@ -140,7 +140,7 @@ def resnet50_16s(input_shape = (224, 224, 3), model_input = ''):
     X = Conv2D(128, (5, 5), strides = (2, 2), name = 'pred_16s', padding = 'valid', kernel_initializer = glorot_uniform(seed=0), kernel_regularizer = regularizers.l2(0.01))(X)
     X = Activation('tanh')(X)
     # merge classifiers
-    X = merge([X, base_model.get_layer('pred_32s').output],mode = 'sum')
+    X = Add([X, base_model.get_layer('pred_32s').output])
     
     #X = AveragePooling2D(pool_size=(2, 2), padding='valid', name='avg_pool')(X)
     
@@ -212,7 +212,7 @@ def resnet50_8s(input_shape = (224, 224, 3), model_input = ''):
     
     # add 16s classifier
     X = base_model.get_layer('activation_10').output
-    X = Conv2D(14, (1, 1), name = 'pred_8', padding = 'valid', kernel_initializer = glorot_uniform(seed=0), kernel_regularizer = regularizers.l2(0.01))(X)
+    X = Conv2D(42, (1, 1), name = 'pred_8', padding = 'valid', kernel_initializer = glorot_uniform(seed=0), kernel_regularizer = regularizers.l2(0.01))(X)
     X = UpSampling2D(name='upsampling_8',size=(int(stride/4), int(stride/4)))(X)
     X = Conv2D(256, (3, 3), strides = (1, 1), name = 'pred_8s_feature1', padding = 'same', kernel_initializer = glorot_uniform(seed=0), kernel_regularizer = regularizers.l2(0.01))(X)
     X = Activation('tanh')(X)
@@ -227,7 +227,7 @@ def resnet50_8s(input_shape = (224, 224, 3), model_input = ''):
     X = Activation('tanh')(X)
 
     # merge classifiers
-    X = merge([X, base_model.get_layer('pred_16s').output],mode = 'sum')
+    X = Add([X, base_model.get_layer('pred_16s').output])
     
     # output layer
     X = Flatten()(X)
